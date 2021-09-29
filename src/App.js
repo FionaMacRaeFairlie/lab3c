0857d860a4e05b69cec1a7e4799b296300b5ebe5
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2021-09-29 15:48:44 +0100

    lab3c files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './colourpallet.scss';
+import React, {Component} from 'react';
+import  Navigation from './nav';
+import './theme.scss';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          theme:"theme-1"
+      };
+      this.handleChange = this.handleChange.bind(this);
+  }
+handleChange(){
+ if (this.state.theme =="theme-1"){this.setState({theme: 'theme-2'});} else {this.setState({theme: 'theme-1'});}
+}
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <span className="container">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>  
+      <div className="box">rr</div> 
+    </span>
+
+    <div className={this.state.theme}>
+      <div className="container"> 
+          <div className="left">   
+              <Navigation />   
+          </div>
+         <div className="right">
+              <button className="button" onClick={ this.handleChange} >Button </button>
+        </div>
+      </div>
     </div>
+    </>
   );
+  }
 }
 
 export default App;
